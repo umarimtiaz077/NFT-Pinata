5c291884bb9eaad751796476344fbcc29415a895
Abdur Rafay Afridi <162041189+rafayafridi786@users.noreply.github.com>
2024-11-17 23:43:58 +0500

    Update detailUser.js

--- pages/detailUser.js
+++ pages/detailUser.js
@@ -14,6 +14,7 @@ import { NFTMarketplaceContext } from "../Context/NFTMarketplaceContext";
 import LikeNFTCardTwo from "../collectionPage/NFTCardTwo/LikeNFTCard";
 import UserTaps from "../authorPage/AuthorTaps/UserTaps";
 import LikeNFTCard from "../collectionPage/NFTCardTwo/MyLikeNFT";
+import FollowerTabCard from "../components/FollowerTab/FollowerTabCard/FollowerTabCard";
 
 const DetailUser = () => {
   const router = useRouter();
